b1b3d2640739523cff62ace8ae7eb3a185325d49
Deep <deepkumarkhamaru@gmail.com>
2021-11-01 10:00:53 +0530

    testing changes nothing major

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,10 @@ for (let iterate = 0; iterate < 52; iterate++) {
 // console.log(ludoBoard);
 // alert("something");
 let ludoColor = ["blue", "red", "green", "yellow"];
+
+let a = [2];
+
+a[0] = 1;
+a[1] = 3;
+
+//console.log(a);
